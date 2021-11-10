650f8995c62da4d88e5fee3994f9d1c7dad43054
Taylor Pridgen <musicinsilence13@gmail.com>
2021-11-10 16:55:16 -0700

    main/ quick stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>MapMeister: AI powered tabletop map maker</title>
+</svelte:head>
+<main>
+  <img src="/MapMeisterLogo.svg" style="width: 45vw" alt="MapMeister logo" />
+</main>
+
+<style>
+  main {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)) 0% 0% / cover,
+      url(/skeleton.png) center center;
+  }
+</style>

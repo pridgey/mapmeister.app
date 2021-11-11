2e22cc8cd7e5ba55a91acffa81c4066e63da67d5
Taylor Pridgen <musicinsilence13@gmail.com>
2021-11-11 10:55:45 -0700

    main/ vercel adapter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
 <svelte:head>
   <title>MapMeister: AI powered tabletop map maker</title>
 </svelte:head>
+<!-- svelte-ignore a11y-media-has-caption -->
+<!-- ignoring this rule because this is a silent video -->
+<video loop autoplay>
+  <source src="/BgVideo.mp4" type="video/mp4" />
+</video>
 <main>
   <img src="/MapMeisterLogo.svg" style="width: 45vw" alt="MapMeister logo" />
 </main>
@@ -17,7 +22,15 @@
     gap: 15px;
     align-items: center;
     justify-content: center;
-    background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)) 0% 0% / cover,
-      url(/skeleton.png) center center;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+
+  video {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: -150px;
+    height: 100vh;
   }
 </style>
